0f24da5c0e82a101b2622c3da888b5c2f86d7178
AndresRoot <ro0t@trackebox.local>
2013-04-24 14:15:12 -0500

    Solving

--- euler-1.py
+++ euler-1.py
@@ -1,25 +1,6 @@
-#Problem 1
 def multiplo():
     ac = 0
     for n in range(1,1001):
         if n % 3 == 0 or n % 5 == 0:
             ac += n
     return n
-
-#Problem 2
-def fibonacci():
-    fib = []
-    n = 0
-    m = 1
-    r = 0
-    while n < 41:
-        fib.append(n)
-        r += n 
-        if n == 0:
-            n += 1
-        else:
-            f = m + n
-            m = n
-            n = f
-    return fib
-            
